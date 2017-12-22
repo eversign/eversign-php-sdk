cbd814111f907bfe696df030f0d153a0549ec52e
Dominik Kukacka <dominik.kukacka@gmail.com>
2017-12-22 14:25:43 +0100

    add oauth test

---
+++ sdk/Eversign/Test/OAuthTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Eversign\Test;
+
+use Eversign\Client;
+use Eversign\OAuthTokenRequest;
+
+
+
+class OAuthTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testCall()
+    {
+
+        $client = new Client(null, null, getenv('SDK_TESTING_MOCK_URL') ? getenv('SDK_TESTING_MOCK_URL') : 'http://localhost:8888/api/');
+
+        $client->setOAuthAccessToken('test_oauth_access_token');
+        $client->setSelectedBusinessById(1337);
+
+        $documents = $client->getAllDocuments();
+        echo 'found ' . sizeof($documents) . ' documents';
+
+    }
+}
